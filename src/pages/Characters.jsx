c70e938cbdeaa7503164307e51994a04321b133f
Johanne Khan Malek <johannekhanmalek@Johannes-MacBook-Pro.local>
2023-11-15 16:20:16 +0100

    with axios request characters

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,8 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Characters = () => {
-  return (
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://site--marvel-backend--j7xsyk95scmh.code.run/characters"
+        );
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
     <div className="container">
       <div className="container-characters">
-        <p>Je suis sur la page Character</p>
+        {data.results.map((character) => {
+          return (
+            <div className="one-character" key={character.name}>
+              <img
+                src={`${character.thumbnail.path}/portrait_xlarge.${character.thumbnail.extension}`}
+                alt=""
+              />
+
+              <div className="one-character-text">
+                <p>{character.name}</p>
+                <p>{character.description}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
