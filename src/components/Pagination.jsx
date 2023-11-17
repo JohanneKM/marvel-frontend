112a51a7970f48be4336bfdca5ab2a7e4407b3bd
Johanne Khan Malek <johannekhanmalek@Johannes-MacBook-Pro.local>
2023-11-17 09:27:35 +0100

    test pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const Pagination = () => {
+  const [currentPage, setCurrentPage] = useState(5);
+  const [resultsPerPage] = useState(100);
+  const nbPages = Math.ceil(1493 / resultsPerPage);
+  const pageNumbers = [...Array(16).keys()].slice(1);
+
+  const goToNextPage = () => {
+    if (currentPage !== nbPages) setCurrentPage(currentPage + 1);
+  };
+
+  const goToPrevPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
+  return (
+    <nav>
+      <ul className="pagination justify-content-center">
+        <li className="page-item">
+          <a className="page-link" onClick={goToPrevPage} href="#">
+            Previous
+          </a>
+        </li>
+        {pageNumbers.map((pgNumber) => (
+          <li
+            key={pgNumber}
+            className={`page-item ${currentPage == pgNumber ? "active" : ""} `}
+          >
+            <a
+              onClick={() => setCurrentPage(pgNumber)}
+              className="page-link"
+              href="#"
+            >
+              {pgNumber}
+            </a>
+          </li>
+        ))}
+        <li className="page-item">
+          <a className="page-link" onClick={goToNextPage} href="#">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
